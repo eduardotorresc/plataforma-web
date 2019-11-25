9efa4540e4f61179624c3179ca807a6fc99f013d
kevintorresc <kevintc47@hotmail.com>
2019-11-25 02:27:31 -0500

    Cambios en los archivos del directorio src/js/ * Se renombran e intercambian los nombres de los archivos menu.js y tabs.js

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,17 +1,11 @@
-const tabsFunctions = () => {
-  let tabs = Array.prototype.slice.apply(document.querySelectorAll('.tabs__item'))
-  let panels = Array.prototype.slice.apply(document.querySelectorAll('.panels__item'))
-  if (document.getElementById('tabs')) {
-    document.getElementById('tabs').addEventListener('click', e => {
-      if (e.target.tagName == 'LI') {
-        let i = tabs.indexOf(e.target)
-        tabs.map(tab => tab.classList.remove('active'))
-        tabs[i].classList.add('active')
-        panels.map(panels => panels.classList.remove('active'))
-        panels[i].classList.add('active')
-      }
-    })
-  }
+const menuFunctions = () => {
+  let toggle = document.getElementById('toggle-menu'),
+    nav = document.getElementById('nav')
+
+  toggle.addEventListener('click', () => {
+    nav.classList.toggle('show'),
+      body.classList.toggle('show')
+  })
 }
 
-tabsFunctions()
+menuFunctions()
